07a5506072a75c1d7dcff133973e2e5e13eef377
khalid423959 <khalidhassanhim@gmail.com>
2023-09-12 16:50:49 -0600

    Blogs api,header commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blogs from './assets/Components/Blogs/Blogs'
+import Header from './assets/Components/Header'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Header></Header>
+      <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl bg-red-500'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
